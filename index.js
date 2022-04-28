3de86f9393f2da5b8c24a120d160867574a6f253
shaunvan119 <shaunvan@live.com.au>
2022-04-28 10:55:37 +1000

    call back function handle click

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
-document.getElementById("new-deck").addEventListener("click", function() {
+function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => console.log(data))
-}) 
+}
+
+document.getElementById("new-deck").addEventListener("click", handleClick)

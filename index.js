6fb553d59d885c9a3d7be9ecd192f6764248ac45
shaunvan119 <shaunvan@live.com.au>
2022-04-29 14:37:37 +1000

    draw card api and rendering images to the dom

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ function handleClick() {
 
 document.getElementById("new-deck").addEventListener("click", handleClick)
 
+document.getElementById("draw-cards").addEventListener("click", () => {
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.cards)
+           document.getElementById("cards").innerHTML = `
+           <img src="${data.cards[0].image}"/>
+           `
+        })
+    
+})
+
+
+

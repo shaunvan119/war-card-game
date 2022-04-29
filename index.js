243a175cb67e9ae53b336804dcaad21143ef7075
shaunvan119 <shaunvan@live.com.au>
2022-04-29 10:58:46 +1000

    deck ID varible

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+let deckId
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data)
+            deckId = data.deck_id
+        })
 }
 
 document.getElementById("new-deck").addEventListener("click", handleClick)
+

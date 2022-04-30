5ea063bf7abf955d56b16d34ec6d0e25debb92f9
shaunvan119 <shaunvan@live.com.au>
2022-04-30 11:44:40 +1000

    refactored code

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
 let deckId
+const cardsContainer = document.getElementById("cards")
+const newDeckBtn = document.getElementById("new-deck")
+const drawCardBtn = document.getElementById("draw-cards")
+const header = document.getElementById("header")
+const remainingText = document.getElementById("remaining")
+
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
-            console.log(data)
+            remainingText.textContent = `Remaining cards: ${data.remaining}`
             deckId = data.deck_id
+            console.log(deckId)
         })
 }
 
-document.getElementById("new-deck").addEventListener("click", handleClick)
+newDeckBtn.addEventListener("click", handleClick)
 
-document.getElementById("draw-cards").addEventListener("click", () => {
+
+drawCardBtn.addEventListener("click", () => {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
-            console.log(data.cards)
-            document.getElementById("cards").children[0].innerHTML = `
+            remainingText.textContent = `Remaining cards: ${data.remaining}`
+            cardsContainer.children[0].innerHTML = `
                 <img src=${data.cards[0].image} class="card" />
             `
-            document.getElementById("cards").children[1].innerHTML = `
+            cardsContainer.children[1].innerHTML = `
                 <img src=${data.cards[1].image} class="card" />
             `
+            const winnerText = determineCardWinner(data.cards[0], data.cards[1])
+            header.textContent = winnerText
+
+            if (data.remaining === 0) {
+                drawCardBtn.disabled = true
+            }
         })
 })
 
@@ -30,22 +44,12 @@ function determineCardWinner(card1, card2) {
     "10", "JACK", "QUEEN", "KING", "ACE"]
     const card1ValueIndex = valueOptions.indexOf(card1.value)
     const card2ValueIndex = valueOptions.indexOf(card2.value)
-    console.log("card 1:", card1ValueIndex)
-    console.log("card 2:", card2ValueIndex)
-if (card1ValueIndex > card2ValueIndex) {
-        console.log("Card 1 wins!")
+    
+    if (card1ValueIndex > card2ValueIndex) {
+        return "Card 1 wins!"
     } else if (card1ValueIndex < card2ValueIndex) {
-        console.log("Card 2 wins!")
+        return "Card 2 wins!"
     } else {
-        console.log("It's a tie!")
+        return "War!"
     }
 }
-
-const card1Obj = {
-    value: "7"
-}
-const card2Obj = {
-    value: "KING"
-}
-
-determineCardWinner(card1Obj, card2Obj)
